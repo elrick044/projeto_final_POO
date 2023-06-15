c123d5e8ee96f6956107625fb9c6f52ad5a8da97
elrick046 <87386997+elrick046@users.noreply.github.com>
2023-06-15 15:57:47 -0300

    efcor + repository

---
+++ projeto_final_POO2/efcore/efcore.cs
@@ -0,0 +1,72 @@
+﻿ using Microsoft.EntityFrameworkCore;
+
+public class Professor
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Login { get; set; }
+    public string Senha { get; set; }
+}
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Login { get; set; }
+    public string Senha { get; set; }
+    
+    public List<Materia> Materias { get; set; }
+}
+
+public class Materia
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    //public int IdProfessor { get; set; }
+    public Professor Professor { get; set; }
+    
+    public List<Aluno> Alunos { get; set; }
+}
+
+public class Prova
+{
+    public int Id { get; set; }
+    //public int IdMateria { get; set; }
+    public DateTime Prazo { get; set; }
+    public float Peso { get; set; }
+    public string NomeProva { get; set; }
+    public Materia Materia { get; set; }
+}
+
+public class Questao
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public float Peso { get; set; }
+    //public int IdProva { get; set; }
+    public Prova Prova { get; set; }
+}
+
+public class Alternativa
+{
+    public int Id { get; set; }
+    //public int IdQuestao { get; set; }
+    public string Texto { get; set; }
+    public bool Correta { get; set; }
+    public Questao Questao { get; set; }
+}
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Professor> Professors { get; set; }
+    public DbSet<Professor> Alunos { get; set; }
+    public DbSet<Materia> Materias { get; set; }
+    public DbSet<Prova> Provas { get; set; }
+    public DbSet<Questao> Questoes { get; set; }
+    public DbSet<Alternativa> Alternativas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySQL("Server=localhost;Port=3306;Database=projeto_final_POO;Uid=root;Pwd=260405;"); 
+    }
+}
---
+++ projeto_final_POO2/repository/repository.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> GetById(int id);
+    Task<List<T>> GetAll();
+    Task Add(T entity);
+    Task Update(T entity);
+    Task Delete(T entity);
+}
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _context;
+
+    public Repository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<T> GetById(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<List<T>> GetAll()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task Add(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+}
+
+public interface IProfessorRepository : IRepository<Professor>
+{
+    // Métodos específicos para o Professor, se necessário
+}
+
+public interface IAlunoRepository : IRepository<Aluno>
+{
+    // Métodos específicos para o Aluno, se necessário
+}
+
+public interface IMateriaRepository : IRepository<Materia>
+{
+    // Métodos específicos para a Matéria, se necessário
+}
+
+public interface IProvaRepository : IRepository<Prova>
+{
+    // Métodos específicos para a Prova, se necessário
+}
+
+public interface IQuestaoRepository : IRepository<Questao>
+{
+    // Métodos específicos para a Questão, se necessário
+}
+
+public interface IAlternativaRepository : IRepository<Alternativa>
+{
+    // Métodos específicos para a Alternativa, se necessário
+}
+
+public class ProfessorRepository : Repository<Professor>, IProfessorRepository
+{
+    public ProfessorRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
+
+public class AlunoRepository : Repository<Aluno>, IAlunoRepository
+{
+    public AlunoRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
+
+public class MateriaRepository : Repository<Materia>, IMateriaRepository
+{
+    public MateriaRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
+
+public class ProvaRepository : Repository<Prova>, IProvaRepository
+{
+    public ProvaRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
+
+public class QuestaoRepository : Repository<Questao>, IQuestaoRepository
+{
+    public QuestaoRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
+
+public class AlternativaRepository : Repository<Alternativa>, IAlternativaRepository
+{
+    public AlternativaRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
